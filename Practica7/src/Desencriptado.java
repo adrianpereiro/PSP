0fc46c4dd45c816397f049524a641d92e65f7283
adrianpereiro <adrianpereiro99@gmail.com>
2021-03-04 10:09:41 +0100

    .

--- Practica7/src/Desencriptado.java
+++ Practica7/src/Desencriptado.java
@@ -10,25 +10,19 @@ public class Desencriptado {
 
 	public static void main(String[] args) {
 		try {
-			// RECUPERAMOS CLAVE SECRETA DEL FICHERO
 			ObjectInputStream oin = new ObjectInputStream(new FileInputStream("C:\\Users\\PC33\\git\\PSP\\Practica7\\src\\clave.ken"));
 			Key clavesecreta = (Key) oin.readObject();
 			oin.close();
 
-			// SE DEFINE EL OBJETO Cipher para desencriptar
 			Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			c.init(Cipher.DECRYPT_MODE, clavesecreta);
 
-			// OBJETO CipherlnputStream CUYO CONTENIDO SE VA A DESCIFRAR
 			CipherInputStream in = new CipherInputStream(new FileInputStream("C:\\Users\\PC33\\git\\PSP\\Practica7\\src\\Fichero.Cifrado"), c);
 			int tambloque = c.getBlockSize();
 			byte[] bytes = new byte[tambloque];
 
-			// FICHERO CON EL CONTENIDO DESCIFRADO QUE SE CREARÁ
 			FileOutputStream fileout = new FileOutputStream("C:\\Users\\PC33\\git\\PSP\\Practica7\\src\\FICHEROdescifrado.pdf");
 
-			// LEEMOS BLOQUES DE BYTES DEL FICHERO cifrado
-			// Y LO VAMOS ESCRIBIENDO desencriptados al FileOutputStream
 			int i = in.read(bytes);
 			while (i != -1) {
 				fileout.write(bytes, 0, i);
@@ -40,7 +34,7 @@ public class Desencriptado {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}// main
+	}
 
 	}
 
