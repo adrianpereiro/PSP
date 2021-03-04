cfacd4c27400b6324a4c58928501d15379f9df18
adrianpereiro <adrianpereiro99@gmail.com>
2021-03-04 12:38:48 +0100

    .

--- Practica3Proceso/src/practica3.java
+++ Practica3Proceso/src/practica3.java
@@ -47,16 +47,15 @@ public class practica3 {
 
 				BufferedReader stdError = new BufferedReader(new 
 				     InputStreamReader(proc.getErrorStream()));
-			
-				// Read the output from the command
-				System.out.println("Here is the standard output of the command:\n");
+
+				System.out.println("Resutado:\n");
 				String s = "";
 				while ((s = stdInput.readLine()) != null) {
 				    System.out.println(s);
 				}
 				
-				// Read any errors from the attempted command
-				System.out.println("Here is the standard error of the command (if any):\n");
+
+				System.out.println("Errores:\n");
 				while ((s = stdError.readLine()) != null) {
 				    System.out.println(s);
 				}
